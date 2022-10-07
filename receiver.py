49f0a288e69eb9c42f0fa55e91101c5929b19741
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-10-07 12:42:30 -0700

    more changes

--- receiver.py
+++ receiver.py
@@ -2,9 +2,9 @@ from scapy.all import sniff, packet
 
 def write_info(pkt):
     with open("log.txt", "a") as f:
-        # f.write(f"{chr(int(pkt.id / 255))}")
-        print(pkt.show())
-        print(pkt)
+        f.write(f"{chr(int(pkt.id / 255))}")
+        # print(pkt.show())
+        # print(pkt)
 
 if __name__ == "__main__":
     sniff(filter="tcp and src host 10.0.0.159", prn=write_info)
