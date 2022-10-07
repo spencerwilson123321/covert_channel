bf340c16d709229889ccaf0dfeb643d60eaeb379
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-10-07 15:28:05 -0700

    changed the way that receiver.py ends execution

--- receiver.py
+++ receiver.py
@@ -5,9 +5,7 @@ def write_info(pkt):
         f.write(f"{chr(int(pkt.id / 255))}")
 
 if __name__ == "__main__":
-    try:
-        sniff(filter="tcp and src host 10.0.0.159", prn=write_info)
-    except KeyboardInterrupt:
-        with open("log.txt", "a") as f:
-            f.write("\n")
-        print("Data written to ./log.txt")
+    sniff(filter="tcp and src host 10.0.0.159", prn=write_info)
+    with open("log.txt", "a") as f:
+        f.write("\n")
+    print("Data written to ./log.txt")
