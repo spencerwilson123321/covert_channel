571016d83fe3ec1cb1ca56271afeefadeb6f678a
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-10-07 15:17:52 -0700

    changed the way that receiver.py works

--- receiver.py
+++ receiver.py
@@ -1,9 +1,6 @@
 from scapy.all import sniff, packet
 
-pkt_counter = 0
-
 def write_info(pkt):
-    pkt_counter += 1
     with open("log.txt", "a") as f:
         f.write(f"{chr(int(pkt.id / 255))}")
 
@@ -11,5 +8,4 @@ if __name__ == "__main__":
     try:
         sniff(filter="tcp and src host 10.0.0.159", prn=write_info)
     except KeyboardInterrupt:
-        print(f"Packets Received: {pkt_counter}")
         print("Data written to ./log.txt")
